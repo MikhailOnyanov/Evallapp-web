4764b615082096a04fcd123d47260502a49f436a
Michael <68405115+Mishania12let@users.noreply.github.com>
2023-06-18 23:59:44 +0500

    v5 - read, update, delete

--- evallapp/data.py
+++ evallapp/data.py
@@ -1,23 +1,21 @@
 import logging
 
 from flask import (
-    Blueprint, render_template, request, current_app, jsonify
+    Blueprint, render_template, request, current_app, jsonify, abort
 )
 from sqlalchemy import inspect
 
 from . import db_session
 from .models.core import *
-from .utils import get_table_by_name, prepare_columns_gridjs_format
+from .utils import get_table_by_name, prepare_columns_gridjs_format, find_mapper_for_table
 
 bp = Blueprint('data', __name__, url_prefix='/api')
 
 
-# returns
 @bp.route('/fetch_dataset', methods=['GET'])
 def fetch_dataset():
     db_name = request.args.get('db_name')
     if db_name:
-        print("privet")
         table_values = get_table_by_name(db_name, prepare_columns=True)
         print(table_values)
         return jsonify(table_values)
@@ -25,45 +23,25 @@ def fetch_dataset():
         return "Dataset not found", 400
 
 
-@bp.route('/data', methods=['GET'])
-def data():
-    query = db_session.query(Works)
-    total = query.count()
+@bp.route('/data', methods=['POST'])
+def update():
+    data = request.get_json()
+    if 'table_name' not in data:
+        abort(400)
+    print(data)
+    t = find_mapper_for_table(Base, data['table_name'])
+    mapped_object = db_session.query(t).get(data["id"])
+    data_to_change = data['data']
+    atr_key = list(data_to_change.keys())[0]
+    data_to_write = data_to_change[atr_key]
+    setattr(mapped_object, atr_key, data_to_write)
+    db_session.commit()
 
-    # search filter
-    search = request.args.get('search')
-    if search:
-        query = query.filter(db.or_(
-            Works.year.like(f'%{search}%'),
-            Works.education_profile.like(f'%{search}%')
-        ))
-    total = query.count()
 
-    # sorting
-    sort = request.args.get('sort')
-    if sort:
-        pass
-        order = []
-        for s in sort.split(','):
-            direction = s[0]
-            name = s[1:]
-            if name not in ['name', 'age', 'email']:
-                name = 'name'
-            col = getattr(User, name)
-            if direction == '-':
-                col = col.desc()
-            order.append(col)
-        if order:
-            query = query.order_by(*order)
-
-    # pagination
-    start = request.args.get('start', type=int, default=-1)
-    length = request.args.get('length', type=int, default=-1)
-    if start != -1 and length != -1:
-        query = query.offset(start).limit(length)
-
-    r = {
-        'data': [work.to_dict() for work in query],
-        'total': total,
-    }
-    return r
+    # user = User.query.get(data['id'])
+    #
+    # for field in ['name', 'age', 'address', 'phone', 'email']:
+    #     if field in data:
+    #         setattr(user, field, data[field])
+    # db.session.commit()
+    return '', 204
